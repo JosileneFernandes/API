3b9024272377c2b36b19f113809655531c49946a
Cleiyton <cleiyton2020@gmail.com>
2018-07-12 23:10:39 -0300

    permisao

--- node-srt/src/app.js
+++ node-srt/src/app.js
@@ -28,6 +28,14 @@ const cadastroRoute = require('./routes/cadastro-route');
 const pedidoRoute = require('./routes/pedidos-route');
 
 app.use(bodyParse.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT,DELETE');
+    next();
+});
 app.use(bodyParse.urlencoded({ 
     extended: false 
 }));
